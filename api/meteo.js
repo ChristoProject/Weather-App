7c6380c670eb106a6f1139445b52c00c2998b00c
Christian <71429861+ChristoProject@users.noreply.github.com>
2025-03-25 14:30:11 +0100

    Update meteo.js

--- api/meteo.js
+++ api/meteo.js
@@ -4,7 +4,7 @@ export class MeteoAPI {
   static async fetchWeatherByCoords(coords) {
     return (
       await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=b9a5d809d5cb9d741ccfa2a152ac0046&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=&units=metric`
       )
     ).data;
   }
